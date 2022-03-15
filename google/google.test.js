1a072ccc0bf00119f5b142fe9829c7bd7cce5629
Alex Lamb <alexlamb104@gmail.com>
2022-03-15 17:56:55 -0600

    Added search for Devmountain building in maps.

--- google/google.test.js
+++ google/google.test.js
@@ -17,25 +17,42 @@ afterAll(async () => {
 
 // This test will require you to change some values in order to get your tests up and running
 // You'll want to change each of the commented out pieces below
-test('I can search Google', async () => {
+// test('I can search Google', async () => {
 
-    // For this line you'll need to put the name of the search bar which you can find by inspecting that element in Chrome
-    let searchBar = await driver.findElement(By.name('q'))
+//     // For this line you'll need to put the name of the search bar which you can find by inspecting that element in Chrome
+//     let searchBar = await driver.findElement(By.name('q'))
     
-    // This time you'll just finish this string with something you want to search on Google, make sure you leanve that \n !
-    await searchBar.sendKeys('Hamilton\n')
+//     // This time you'll just finish this string with something you want to search on Google, make sure you leanve that \n !
+//     await searchBar.sendKeys('Hamilton\n')
 
-    // This line will pause our automation so you can see your search!
-    await driver.sleep(2000)
+//     // This line will pause our automation so you can see your search!
+//     await driver.sleep(2000)
 
-    // We now want our automation to get the search bar again since we're on a new page
-    searchBar = await driver.findElement(By.name('q'))
+//     // We now want our automation to get the search bar again since we're on a new page
+//     searchBar = await driver.findElement(By.name('q'))
 
-    // We'll use this line to clear out our old input.
-    await searchBar.clear() 
+//     // We'll use this line to clear out our old input.
+//     await searchBar.clear() 
 
-    // Let's do another search!
-    await searchBar.sendKeys('Utah Jazz\n')
+//     // Let's do another search!
+//     await searchBar.sendKeys('Utah Jazz\n')
 
-    await driver.sleep(2000) 
+//     await driver.sleep(2000) 
+// })
+
+
+let address = '1550 Digital Dr, Lehi, UT 84043'
+let hide = 'Yr7JMd-pane-ornU0b-LgbsSe noprint'
+test("Search an address on google maps ", async () => {
+    let searchBar  = await driver.findElement(By.name('q'))
+
+    await searchBar.clear()
+
+    await searchBar.sendKeys(`${address}\n`)
+
+    await driver.sleep(3000)
+
+    await driver.findElement(By.className('wpk9re')).click()
+
+    await driver.sleep(5000)    
 })
